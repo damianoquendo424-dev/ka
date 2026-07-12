01e313ef4b647df0f5ca481d736c722d917cca72
damianoquendo424-dev <damianoquendo424@gmail.com>
2026-07-12 13:29:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import secrets
 app = Flask(__name__)
 
 # ===== CHANGE THESE =====
-ADMIN_USERNAME = 'admin'
-ADMIN_PASSWORD = 'admin'
+ADMIN_USERNAME = 'deu'
+ADMIN_PASSWORD = 'deu'
 # =======================
 
 app.config['SECRET_KEY'] = 'secret-key-change-me'
